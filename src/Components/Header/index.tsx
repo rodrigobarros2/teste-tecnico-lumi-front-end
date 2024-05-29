54c04c1d0fd338a5340b125914c7199c677a1cfd
Rodrigo Barros <rodrigobarros110@hotmail.com>
2024-05-29 05:55:29 -0300

    feat: fiz name

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <header className="bg-green-950 text-white py-6 px-6">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <h1 className="text-3xl font-bold">Lumi Energia</h1>
         <div className="flex items-center space-x-4"></div>
       </div>
     </header>
