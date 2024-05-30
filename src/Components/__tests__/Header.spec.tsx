680c7b24b839e53b7829242cf98ff0273f2e3d67
Rodrigo Barros <rodrigobarros110@hotmail.com>
2024-05-30 02:59:21 -0300

    Update Header.spec.tsx

--- src/Components/__tests__/Header.spec.tsx
+++ src/Components/__tests__/Header.spec.tsx
@@ -1,24 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from "../Header";
+import Logo from "../../assets/logo.svg";
 
-test("Should be header component with correct text and styles", () => {
+test("Should render header component with correct text and styles", () => {
   render(<Header />);
-
-  const titleElement = screen.getByText(/Dashboard/i);
-  expect(titleElement).toBeInTheDocument();
 
   const headerElement = screen.getByRole("banner");
   expect(headerElement).toHaveClass("bg-green-950");
-
   expect(headerElement).toHaveClass("text-white");
+  expect(headerElement).toHaveClass("py-6");
+  expect(headerElement).toHaveClass("px-6");
 
-  const containerElement = screen.getByRole("banner").firstChild;
+  const containerElement = headerElement.firstChild;
   expect(containerElement).toHaveClass("container");
   expect(containerElement).toHaveClass("mx-auto");
   expect(containerElement).toHaveClass("flex");
   expect(containerElement).toHaveClass("justify-between");
   expect(containerElement).toHaveClass("items-center");
 
-  const headingElement = screen.getByRole("heading");
-  expect(headingElement).toHaveTextContent("Dashboard");
+  const logoElement = screen.getByAltText("Logo");
+  expect(logoElement).toBeInTheDocument();
+  expect(logoElement).toHaveClass("w-20");
+  expect(logoElement).toHaveAttribute("src", Logo);
 });
